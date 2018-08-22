a0570fd9083e55052349003493528f3629a4b38b
Travis Jones <travisj_jones@hotmail.com>
2018-08-21 19:51:27 -0500

    added Router to index.js and a NotFound component for URLs not specific to app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+
 import './css/style.css';
 
-import App from './components/App'
+import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/" component={StorePicker} />
+          <Route path="/store/:storeId" component={App} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-render(<App />, document.querySelector('#main'))
+render(<Root />, document.querySelector('#main'));
