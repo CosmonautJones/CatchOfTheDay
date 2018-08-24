ba8946fcff5bfab9960f2241ecde1218b074be62
Travis Jones <travisj_jones@hotmail.com>
2018-08-24 12:17:47 -0500

    setting up menu form to add items to menu
    also set up the store redirect from StorePicker.js

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // event is all the data
+  goToStore(event) {
+    // prevents the broswer from doing its default action such as sending data in form/refreshing
+    event.preventDefault();
+    // grab text from box
+    const storeId = this.storeInput.value;
+    console.log(storeId);
+    // tranistion URL from '/' to '/store/:storeId'
+    this.props.history.push(`/store/${storeId}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" requried placeholder="Store Name" />
+        <input
+          type="text"
+          required={true}
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={input => {
+            this.storeInput = input;
+          }}
+        />
         <button type="submit">Visit Store >></button>
       </form>
-    )
+    );
   }
 }
 
